119ba0e93a0ab8cb5567cc526a2d51a45134af1a
DaubieMatthieu <matthieu.daubie@isep.fr>
2021-12-08 17:16:27 +0100

    ApiRepository main architecture with basic version of search method

--- app/src/main/java/com/example/pimpmymeal/model/Recipe.java
+++ app/src/main/java/com/example/pimpmymeal/model/Recipe.java
@@ -1,10 +1,13 @@
 package com.example.pimpmymeal.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 //TODO complete
 public class Recipe {
     public final String name;
 
-    public Recipe(String name) {
-        this.name = name;
+    public Recipe(JSONObject jsonObject) throws JSONException {
+        name = jsonObject.getString("label");
     }
 }
