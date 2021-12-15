d68bd6b84e45f57ec510968ecdc32ed9903d0f7e
charlieHeinze <A01700179@itesm.mx>
2021-12-15 11:54:34 +0100

    Created DBHelper Test

---
+++ app/src/main/java/com/example/pimpmymeal/DBHelper.java
@@ -0,0 +1,30 @@
+package com.example.pimpmymeal;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "login.db";
+    public DBHelper(Context context) {
+        super(context, DBNAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table users(username TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists users");
+    }
+
+    public boolean insertData(String username, String password)
+    {
+        
+    }
+}
