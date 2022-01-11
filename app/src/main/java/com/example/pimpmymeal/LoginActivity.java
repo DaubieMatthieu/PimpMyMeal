28d321023230dd48ad52c7dad73b28c470f02bf8
charlieHeinze <A01700179@itesm.mx>
2022-01-11 15:11:02 +0100

    Update LoginActivity.java

--- app/src/main/java/com/example/pimpmymeal/LoginActivity.java
+++ app/src/main/java/com/example/pimpmymeal/LoginActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.pimpmymeal.ui.HomeActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private final String usernameGeneric = "user";
+    private final String passwordGeneric = "12345";
 
     EditText username, password, repassword;
     Button signup, signin;
@@ -29,6 +33,25 @@ public class LoginActivity extends AppCompatActivity {
 
     public void SignUpClicked(View v)
     {
+        String inputUsername = username.getText().toString();
+        String inputPassword = password.getText().toString();
+
+        if(inputUsername.isEmpty() || inputPassword.isEmpty())
+        {
+            Toast.makeText(this, "Don´t leave fields in blank", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            if(validateEntrance(inputUsername, inputPassword))
+            {
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+            }
+        }
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
@@ -37,5 +60,15 @@ public class LoginActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(this, LoginActivitySignIn.class);
         startActivity(intent);
+    }
+
+    private boolean validateEntrance(String name, String pass)
+    {
+        if(name.equals(usernameGeneric) && pass.equals(passwordGeneric))
+        {
+            return true;
+        }
+        else
+            return false;
     }
 }
