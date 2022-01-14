c985caa2c9ab0db5505ee6ec4c82d7eaad175ece
DaubieMatthieu <matthieu.daubie@isep.fr>
2022-01-14 19:34:48 +0100

    Recipe Activity v1

--- app/src/main/java/com/example/pimpmymeal/model/Recipe.java
+++ app/src/main/java/com/example/pimpmymeal/model/Recipe.java
@@ -9,7 +9,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 // If a corresponding Ingredient object is implemented, it will need to be serializable as well
 public class Recipe implements Serializable {
@@ -19,7 +18,7 @@ public class Recipe implements Serializable {
     private transient Bitmap image;
     public final String source;
     public final String sourceUri;
-    public final ArrayList<String> ingredients = new ArrayList<>();
+    public final String[] ingredients;
 
     public Recipe(JSONObject jsonObject) throws JSONException {
         this(jsonObject, true);
@@ -35,9 +34,9 @@ public class Recipe implements Serializable {
         JSONArray ingredientsJson = jsonObject.getJSONArray("ingredientLines");
         // Later on we might want to use the "ingredients" key instead
         // which gives ingredients as objects (so with more information on each one).
-        for (int i = 0; i < ingredientsJson.length(); i++) {
-            ingredients.add(ingredientsJson.getString(i));
-        }
+        ingredients = new String[ingredientsJson.length()];
+        for (int i = 0; i < ingredientsJson.length(); i++)
+            ingredients[i] = ingredientsJson.getString(i);
         // More info can be extracted from the json response.
     }
 
